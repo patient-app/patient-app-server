74fe202de62ba038cef2faf8d148e70fa8611f55
Lukas Niedhart <lukas.niedhart@gmx.ch>
2025-03-13 13:17:47 +0100

    Initial commit

---
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/PatientAppApplication.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.imrg.patientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/uzh/ifi/imrg/patientapp/PatientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.imrg.patientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
