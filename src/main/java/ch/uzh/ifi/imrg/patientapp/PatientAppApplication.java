6a53ddd576b10acd98447848af2efdb62be420a6
Jonas Blum <jonas.blum2@uzh.ch>
2025-03-16 10:28:39 +0100

    .

--- src/main/java/ch/uzh/ifi/imrg/patientapp/PatientAppApplication.java
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/PatientAppApplication.java
@@ -2,11 +2,15 @@ package ch.uzh.ifi.imrg.patientapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RestController
 @SpringBootApplication
@@ -20,6 +24,26 @@ public class PatientAppApplication {
 	@ResponseStatus(HttpStatus.OK)
 	public String helloWorld() {
 		return "The application is running.";
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/**")
+						.allowedOriginPatterns("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
