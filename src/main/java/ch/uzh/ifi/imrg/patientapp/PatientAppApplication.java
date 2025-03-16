7299391b0a924e25efe519123d9743bc544d59c8
Jonas Blum <jonas.blum2@uzh.ch>
2025-03-16 08:49:54 +0100

    Adding / endpoint

--- src/main/java/ch/uzh/ifi/imrg/patientapp/PatientAppApplication.java
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/PatientAppApplication.java
@@ -2,12 +2,24 @@ package ch.uzh.ifi.imrg.patientapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class PatientAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientAppApplication.class, args);
+	}
+
+	@GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public String helloWorld() {
+		return "The application is running.";
 	}
 
 }
