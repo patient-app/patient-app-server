29a944ea8064020f75af8163ed36c451de31bcd8
SpaghettiCodeKing <eliassuter@bluewin.ch>
2025-05-09 18:36:01 +0200

    updated errors.

--- src/main/java/ch/uzh/ifi/imrg/patientapp/service/PatientService.java
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/service/PatientService.java
@@ -37,7 +37,7 @@ public class PatientService {
             return foundPatient;
         }
         throw new ResponseStatusException(
-                HttpStatus.UNAUTHORIZED, "Patient could not be found for the provided JWT");
+                HttpStatus.UNAUTHORIZED, "Client could not be found. Please check, username and password");
     }
 
     public Patient addConversationToPatient(Patient patient, Conversation conversation) {
@@ -52,22 +52,18 @@ public class PatientService {
             HttpServletRequest httpServletRequest,
             HttpServletResponse httpServletResponse) {
         if (patient.getEmail() == null) {
-            throw new Error("Creating patient failed because no email was specified");
+            throw new Error("Creating client failed because no email was specified");
         }
         if (patient.getPassword() == null) {
-            throw new Error("Creating patient failed because no password was specified");
+            throw new Error("Creating client failed because no password was specified");
         }
         if (patient.getId() != null && this.patientRepository.existsById(patient.getId())) {
             throw new Error(
-                    "Creating patient failed because patient with ID "
-                            + patient.getId()
-                            + "already exists");
+                    "Creating client failed because patient with this ID already exists");
         }
         if (patient.getEmail() != null && patientRepository.existsByEmail(patient.getEmail())) {
             throw new Error(
-                    "Creating patient failed because patient with email "
-                            + patient.getEmail()
-                            + "already exists");
+                    "Creating client failed because patient with this email" + patient.getEmail() +"already exists");
         }
 
         patient.setPassword(PasswordUtil.encryptPassword(patient.getPassword()));
@@ -85,12 +81,12 @@ public class PatientService {
             HttpServletResponse httpServletResponse) {
         Patient foundPatient = patientRepository.getPatientByEmail(loginPatientDTO.getEmail());
         if (foundPatient == null) {
-            throw new Error("No therapist with email: " + loginPatientDTO.getEmail() + " exists");
+            throw new Error("No client with email: " + loginPatientDTO.getEmail() + " exists");
         }
 
         if (!PasswordUtil.checkPassword(
                 loginPatientDTO.getPassword(), foundPatient.getPassword())) {
-            throw new Error("The password you entered is wrong!");
+            throw new Error("Wrong password.");
         }
 
         String jwt = JwtUtil.createJWT(loginPatientDTO.getEmail());
