8f9eccbeada0174950ce1b2bb27f7d5f2d914602
Marion Andermatt <marion.andermatt@gmx.ch>
2025-05-14 20:23:30 +0200

    another try to fix sonarqube for backend

---
+++ src/test/java/ch/uzh/ifi/imrg/patientapp/service/PatientServiceTest.java
@@ -0,0 +1,15 @@
+package ch.uzh.ifi.imrg.patientapp.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PatientServiceTest {
+
+    //TODO: remove this test it's just for testing the sonarqube
+    @Test
+    void placeHolder() {
+        assertTrue(true);
+    }
+
+}
