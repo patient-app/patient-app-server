2bd6b16cbec1b42535183ac359e196b84bdabb09
SpaghettiCodeKing <eliassuter@bluewin.ch>
2025-06-15 13:35:00 +0200

    setting sharing with Ai and coach works

--- src/main/java/ch/uzh/ifi/imrg/patientapp/service/ConversationService.java
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/service/ConversationService.java
@@ -3,6 +3,8 @@ package ch.uzh.ifi.imrg.patientapp.service;
 import ch.uzh.ifi.imrg.patientapp.entity.Conversation;
 import ch.uzh.ifi.imrg.patientapp.entity.Patient;
 import ch.uzh.ifi.imrg.patientapp.repository.ConversationRepository;
+import ch.uzh.ifi.imrg.patientapp.rest.dto.input.PutSharingDTO;
+import ch.uzh.ifi.imrg.patientapp.rest.mapper.ConversationMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +28,20 @@ public class ConversationService {
         conversation.setPatient(patient);
         return this.conversationRepository.save(conversation);
     }
+
+    public void updateSharing(PutSharingDTO putSharingDTO, String externalConversationId, Patient loggedInPatient) {
+        Optional<Conversation> optionalConversation = conversationRepository.getConversationByExternalId(externalConversationId);
+        Conversation conversation;
+        if (optionalConversation.isPresent()) {
+            conversation = optionalConversation.get();
+        }else {
+            throw new NoSuchElementException("No conversation found with external ID: " + externalConversationId);
+        }
+        authorizationService.checkConversationAccess(conversation, loggedInPatient, "You can't set access rights for chats of a different user.");
+        ConversationMapper.INSTANCE.updateConversationFromPutSharingDTO(putSharingDTO, conversation);
+        conversationRepository.save(conversation);
+    }
+
     public Conversation getAllMessagesFromConversation(String externalConversationId, Patient patient) {
         Optional<Conversation> optionalConversation = this.conversationRepository.getConversationByExternalId(externalConversationId);
 
