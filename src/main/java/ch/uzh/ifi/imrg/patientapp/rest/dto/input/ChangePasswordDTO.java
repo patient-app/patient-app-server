acecadbfc0c0d49430ff9c16133bc6dd6f0a1ea3
Lukas Niedhart <lukas.niedhart@gmx.ch>
2025-06-16 16:36:17 +0200

    checks password strength closes #86

--- src/main/java/ch/uzh/ifi/imrg/patientapp/rest/dto/input/ChangePasswordDTO.java
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/rest/dto/input/ChangePasswordDTO.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.imrg.patientapp.rest.dto.input;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +15,7 @@ public class ChangePasswordDTO {
 
     @NotBlank
     @Size(min = 8, message = "New password must be at least 8 characters")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).*$", message = "Password must contain upper, lower and digits")
     private String newPassword;
 
     @NotBlank
