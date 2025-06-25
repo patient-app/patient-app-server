a5342cdc65cacab372a2aec6acd59e64be385aa5
lukasniedh <146735707+lukasniedh@users.noreply.github.com>
2025-06-25 15:46:24 +0200

    revert cookie changes (#309)

--- src/main/java/ch/uzh/ifi/imrg/patientapp/utils/JwtUtil.java
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/utils/JwtUtil.java
@@ -38,8 +38,8 @@ public class JwtUtil {
     cookie.setHttpOnly(true);
     cookie.setMaxAge(30 * 24 * 60 * 60); // 30 days
     cookie.setPath("/");
-    cookie.setSecure(true);
-    cookie.setAttribute("SameSite", "None");
+    cookie.setSecure(request.isSecure());
+    cookie.setAttribute("SameSite", "Lax");
     response.addCookie(cookie);
   }
 
@@ -49,7 +49,7 @@ public class JwtUtil {
     cookie.setMaxAge(0);
     cookie.setPath("/");
     cookie.setSecure(false);
-    cookie.setAttribute("SameSite", "None");
+    cookie.setAttribute("SameSite", "Lax");
     response.addCookie(cookie);
 
   }
