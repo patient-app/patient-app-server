c9e0d82ea22b3630da2d420ad48e723804cce967
SpaghettiCodeKing <108170200+SpaghettiCodeKing@users.noreply.github.com>
2025-06-27 23:18:39 +0200

    Mood tracking  (#323)

--- src/main/java/ch/uzh/ifi/imrg/patientapp/rest/dto/output/exercise/ExerciseInformationOutputDTO.java
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/rest/dto/output/exercise/ExerciseInformationOutputDTO.java
@@ -1,9 +1,11 @@
 package ch.uzh.ifi.imrg.patientapp.rest.dto.output.exercise;
 
+import ch.uzh.ifi.imrg.patientapp.rest.dto.input.exercise.ExerciseMoodInputDTO;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,4 +13,6 @@ public class ExerciseInformationOutputDTO {
     private Instant startTime;
     private Instant endTime;
     private String feedback;
+    private List<ExerciseMoodOutputDTO> moodsBefore;
+    private List<ExerciseMoodOutputDTO> moodsAfter;
 }
