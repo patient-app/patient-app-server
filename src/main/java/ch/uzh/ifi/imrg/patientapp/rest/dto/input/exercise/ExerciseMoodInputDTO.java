053928ac792d5d0991188ea193bbfd84e7924822
SpaghettiCodeKIng <eliassuter2001@gmail.com>
2025-06-27 20:09:23 +0200

    inputDTOs

---
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/rest/dto/input/exercise/ExerciseMoodInputDTO.java
@@ -0,0 +1,20 @@
+package ch.uzh.ifi.imrg.patientapp.rest.dto.input.exercise;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExerciseMoodInputDTO {
+    private String moodName;
+    private int moodScore;
+
+    @Override
+    public String toString() {
+        return "ExerciseMoodInputDTO{" +
+                "moodName='" + moodName + '\'' +
+                ", moodScore=" + moodScore +
+                '}';
+    }
+}
