374a0910c6de4102def54dc60b6cd9338b445434
Lukas Niedhart <lukas.niedhart@gmx.ch>
2025-07-01 10:17:56 +0200

    #9

---
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/repository/DocumentRepository.java
@@ -0,0 +1,15 @@
+package ch.uzh.ifi.imrg.patientapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ch.uzh.ifi.imrg.patientapp.entity.Document.Document;
+
+import java.util.Optional;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document, String> {
+
+    Optional<Document> findBySha256(String sha256);
+
+}
