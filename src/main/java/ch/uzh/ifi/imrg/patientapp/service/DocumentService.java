7935e278c5269564e7b719f7d9bb5b4af9e23427
Lukas Niedhart <lukas.niedhart@gmx.ch>
2025-07-02 11:48:30 +0200

    remove unused import

--- src/main/java/ch/uzh/ifi/imrg/patientapp/service/DocumentService.java
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/service/DocumentService.java
@@ -5,7 +5,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HexFormat;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
