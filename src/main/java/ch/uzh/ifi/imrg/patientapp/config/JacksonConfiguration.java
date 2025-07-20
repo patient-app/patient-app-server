99a0b438de53e771a8bf86308a30b298df0212d6
Jonas Blum <jonas.blum2@uzh.ch>
2025-07-20 16:02:58 +0200

    increasing-api-limits

---
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/config/JacksonConfiguration.java
@@ -0,0 +1,18 @@
+package ch.uzh.ifi.imrg.patientapp.config;
+
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
+        return builder -> builder.postConfigurer(objectMapper -> {
+            objectMapper.getFactory().setStreamReadConstraints(
+                    StreamReadConstraints.builder().maxNumberLength(Integer.MAX_VALUE).build());
+        });
+    }
+}
