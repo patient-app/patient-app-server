cb172234d54b73564e68e88ebee06d40cc4775cb
lukasniedh <146735707+lukasniedh@users.noreply.github.com>
2025-07-23 09:48:47 +0200

    POST journal-entry: allow empty string for title and content (#388)

--- src/main/java/ch/uzh/ifi/imrg/patientapp/rest/dto/input/JournalEntryRequestDTO.java
+++ src/main/java/ch/uzh/ifi/imrg/patientapp/rest/dto/input/JournalEntryRequestDTO.java
@@ -3,6 +3,7 @@ package ch.uzh.ifi.imrg.patientapp.rest.dto.input;
 import java.util.Set;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,11 +12,11 @@ import lombok.Setter;
 @Setter
 public class JournalEntryRequestDTO {
 
-    @NotBlank(message = "Title must not be blank")
+    @NotNull(message = "Title must not be blank")
     @Size(max = 255, message = "Title must be at most 255 characters")
     private String title;
 
-    @NotBlank(message = "Content must not be blank")
+    @NotNull(message = "Content must not be blank")
     private String content;
 
     private Set<@NotBlank(message = "Tag must not be blank") String> tags;
